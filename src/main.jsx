c6a17497a650eeb969b27b7ea097b3f24d4db231
NTAF8891 <arthurnemangou@gmail.com>
2026-07-08 09:46:49 +0000

    analytics fait

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
